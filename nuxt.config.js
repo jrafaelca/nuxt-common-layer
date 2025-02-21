937e455eb30153c08a70fd53730f803ddc779641
Jose Carrizales <jrafaelca@gmail.com>
2025-02-21 09:09:03 -0300

    feat: integrate ESLint into Nuxt with added configurations (#3)
    - Added the `@nuxt/eslint` module to streamline linting and improve code quality.
    - Updated `nuxt.config.js` to include stylistic ESLint configurations.
    - Modified dependencies and `package-lock.json` to include necessary ESLint-related packages.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,14 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// eslint-disable-next-line no-undef
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  modules: ['@nuxt/eslint'],
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
+  },
+  compatibilityDate: '2024-11-01',
+  eslint: {
+    config: {
+      stylistic: true,
+    },
   },
 })
